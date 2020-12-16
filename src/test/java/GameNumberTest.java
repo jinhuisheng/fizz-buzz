bf0cef6da7045c2de99df8618839b6ee439bdde3
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-16 11:33:33 +0800

    empty message

--- src/test/java/GameNumberTest.java
+++ src/test/java/GameNumberTest.java
@@ -5,26 +5,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 class GameNumberTest {
 
     @Test
-    void should_toString_itself() {
+    void should_be_itself() {
         GameNumber gameNumber = new GameNumber(2);
         assertThat(gameNumber.toString()).isEqualTo("2");
     }
 
     @Test
-    void should_toString_Fizz_given_number_divided_by_3() {
+    void should_be_Fizz_given_number_divided_by_3() {
         assertThat(new GameNumber(3).toString()).isEqualTo("Fizz");
         assertThat(new GameNumber(6).toString()).isEqualTo("Fizz");
         assertThat(new GameNumber(18).toString()).isEqualTo("Fizz");
     }
 
     @Test
-    void should_toString_Fizz_given_number_contains_3() {
+    void should_be_Fizz_given_number_contains_3() {
         assertThat(new GameNumber(13).toString()).isEqualTo("Fizz");
         assertThat(new GameNumber(31).toString()).isEqualTo("Fizz");
     }
 
     @Test
-    void should_toString_Buzz_given_number_divided_by_5() {
+    void should_be_Buzz_given_number_divided_by_5() {
         assertThat(new GameNumber(5).toString()).isEqualTo("Buzz");
         assertThat(new GameNumber(10).toString()).isEqualTo("Buzz");
     }
@@ -35,13 +35,13 @@ class GameNumberTest {
     }
 
     @Test
-    void should_toString_FizzBuzz_given_number_divided_by_3_and_5() {
+    void should_be_FizzBuzz_given_number_divided_by_3_and_5() {
         assertThat(new GameNumber(15).toString()).isEqualTo("FizzBuzz");
         assertThat(new GameNumber(45).toString()).isEqualTo("FizzBuzz");
     }
 
     @Test
-    void should_toString_FizzBuzz_given_number_contains_3_and_5() {
+    void should_be_FizzBuzz_given_number_contains_3_and_5() {
         assertThat(new GameNumber(53).toString()).isEqualTo("FizzBuzz");
     }
 

918b00e75ab2dc4064bb7c9d2a5ee0fff3de7414
huisheng.jin <huisheng.jinge@gmail.com>
2021-04-27 12:30:11 +0800

    feat: add exampleTest

---
+++ src/test/java/example/ExampleTest.java
@@ -0,0 +1,14 @@
+package example;
+
+import fizzbuzz.FizzBuzz;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class ExampleTest {
+
+    @Test
+    void should_success() {
+        assertThat(1).isEqualTo(1);
+    }
+}
